1aae87313c55ac114e3a3a4a894f7dfe0b6e1558
Tyler Waltze <twaltze@attentivemobile.com>
2024-02-23 13:36:48 -0500

    Remove commented out path/alias code

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,10 +1,3 @@
-// const path = require('path');
-// const pak = require('../package.json');
-
 module.exports = {
-  dependencies: {
-    // [pak.name]: {
-    //   root: path.join(__dirname, '..'),
-    // },
-  },
+  dependencies: {},
 };
